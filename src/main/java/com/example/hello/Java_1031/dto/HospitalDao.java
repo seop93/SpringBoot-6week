3364e476ec0d334ac5330cc1893e593617340490
byeonheungseob <kevin9160@naver.com>
2022-11-01 13:33:18 +0900

    Dao insert 구문 맵핑하기

---
+++ src/main/java/com/example/hello/Java_1031/dto/HospitalDao.java
@@ -0,0 +1,37 @@
+package com.example.hello.Java_1031.dto;
+
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Component;
+
+@Component
+public class HospitalDao {
+
+    private final JdbcTemplate jdbcTemplate;
+
+    public HospitalDao(JdbcTemplate jdbcTemplate) {
+        this.jdbcTemplate = jdbcTemplate;
+    }
+
+    public void add(Hospital hospital) {
+        String sql = "INSERT INTO `likelion-db`.`nation_wide_hospitals` (`id`, `open_service_name`, `open_local_government_code`, `management_number`, `license_date`, `business_status`, `business_status_code`, `phone`, `full_address`, `road_name_address`, `hospital_name`, `business_type_name`, `healthcare_provider_count`, `patient_room_count`, `total_number_of_beds`, `total_area_size`)" +
+                " VALUES (?,?,?," +
+                " ?,?,?," +
+                " ?,?,?," +
+                " ?,?,?" +
+                " ?,?,?" +
+                " ?);"; // 16개
+
+        this.jdbcTemplate.update(sql,
+                hospital.getId(), hospital.getOpenServiceName(), hospital.getOpenLocalGovermentCode(),
+                hospital.getManagementNumber(), hospital.getLicenseDate(), hospital.getBusinessStatus(),
+                hospital.getBusinessStatusCode(), hospital.getPhone(), hospital.getFullAddress(),
+                hospital.getRoadNameAddress(), hospital.getHospitalName(), hospital.getBusinessTypeName(),
+                hospital.getHealthcareProviderCount(), hospital.getPatientRoomCount(), hospital.getTotalNumberOfBeds(),
+                hospital.getTotalAreaSize()
+        );
+    }
+
+}
+
+
+
